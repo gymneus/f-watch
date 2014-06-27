67531ae9bfbd5183ab34b8d050fbe1b683ba081d
Thedi Stana <t.stana@cern.ch>
2014-06-27 06:36:32 +0200

    sw: Moved Linux-compilable comparison projects to os-comparison/

--- sw/RTX/Examples/RTX_ex1/build/rt_Memory.d
+++
@@ -1,6 +0,0 @@
-build/rt_Memory.o: ../../SRC/rt_Memory.c ../../SRC/rt_TypeDef.h \
- ../../SRC/rt_Memory.h
-
-../../SRC/rt_TypeDef.h:
-
-../../SRC/rt_Memory.h:
---
+++ sw/os-comparison/README.txt
@@ -0,0 +1,2 @@
+FreeRTOS - `make' in freertos_blink/armgcc/
+RTX      - `make' in RTX/Examples/RTX_ex2/
