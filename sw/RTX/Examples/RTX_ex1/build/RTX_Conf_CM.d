e5fc761f29f71d58106c2a7f9be2bcb5c181baa4
Thedi Stana <t.stana@cern.ch>
2014-06-27 06:36:32 +0200

    sw: Moved Linux-compilable comparison projects to os-comparison/

--- sw/RTX/Examples/RTX_ex1/build/RTX_Conf_CM.d
+++
@@ -1,6 +0,0 @@
-build/RTX_Conf_CM.o: RTX_Conf_CM.c ../../INC/cmsis_os.h \
- ../../INC/RTX_CM_lib.h
-
-../../INC/cmsis_os.h:
-
-../../INC/RTX_CM_lib.h:
--- sw/RTX/Examples/RTX_ex1/build/RTX_ex1.d
+++
@@ -1,3 +0,0 @@
-build/RTX_ex1.o: RTX_ex1.c ../../INC/cmsis_os.h
-
-../../INC/cmsis_os.h:
