131c51b74d8c61904809d1378a4aeb0a94e837e5
xpiroux <xavierpiroux@gmail.com>
2014-07-18 00:54:04 +0200

    added SPI implementation provided by EnergyMicro , created file for implementing driver FAT on SD through SPI

---
+++ sw/os-comparison/freertos_blink_fat/src/media-drv/sd/config_mdriver_sd.h
@@ -0,0 +1,26 @@
+/*
+ * TODO : license
+ */
+
+#ifndef _CONFIG_MDRIVER_SD_H_
+#define _CONFIG_MDRIVER_SD_H_
+
+#include "../version/ver_mdriver_sd.h"
+#if VER_MDRIVER_SD_MAJOR != 1 || VER_MDRIVER_SD_MINOR != 0
+ #error Incompatible MDRIVER_SD version number!
+#endif
+
+#define PIN_UC_PC3    12
+#define PIN_UC_PD0    28
+#define PIN_UC_PD1    29
+#define PIN_UC_PD2    30
+#define PIN_UC_PD3    31
+
+#define PIN_UC_SD_MOSI    PIN_UC_PD0
+#define PIN_UC_SD_MISO    PIN_UC_PD1
+#define PIN_UC_SD_CLK     PIN_UC_PD2
+#define PIN_UC_SD_CS_N    PIN_UC_PD3
+
+/* usage : add those pins values to GPIO_BASE */
+
+#endif /* _CONFIG_MDRIVER_SD_H_ */
---
+++ sw/os-comparison/freertos_blink_fat/src/version/ver_mdriver_sd.h
@@ -0,0 +1,12 @@
+/*
+ * TODO : license
+ */
+
+#ifndef _VER_MDRIVER_SD_H
+#define _VER_MDRIVER_SD_H
+
+#define VER_MDRIVER_SD_MAJOR 1
+#define VER_MDRIVER_SD_MINOR 0
+
+#endif /* _VER_MDRIVER_SD_H /*
+
