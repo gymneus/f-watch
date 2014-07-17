64bf43349af94567af92351e45c664052932c432
xpiroux <xavierpiroux@gmail.com>
2014-07-18 01:42:09 +0200

    compilation of SD driver OK (only skeletton right now)

--- sw/os-comparison/freertos_blink_fat/src/media-drv/sd/sd_driver_f.c
+++ sw/os-comparison/freertos_blink_fat/src/media-drv/sd/sd_driver_f.c
@@ -2,25 +2,40 @@
  * TODO : license
  */
 
+
+#include "../../api/api_mdriver_ram.h"
+#include "config_mdriver_sd.h"
+#include "../../psp/include/psp_string.h"
+
+#if VER_MDRIVER_SD_MAJOR != 1 || VER_MDRIVER_SD_MINOR != 0
+ #error Incompatible MDRIVER_SD version number!
+#endif
+
 //TODO: use those CMD to send it through SPI interface
 /* MMC/SD command (in SPI) */
-#define USD_CMD0	(0x40+0)	/* GO_IDLE_STATE */
-#define USD_CMD1	(0x40+1)	/* SEND_OP_COND (MMC) */
-#define	USD_ACMD41	(0xC0+41)	/* SEND_OP_COND (SDC) */
-#define USD_CMD8	(0x40+8)	/* SEND_IF_COND */
-#define USD_CMD9	(0x40+9)	/* SEND_CSD */
-#define USD_CMD10	(0x40+10)	/* SEND_CID */
-#define USD_CMD12	(0x40+12)	/* STOP_TRANSMISSION */
-#define USD_ACMD13	(0xC0+13)	/* SD_STATUS (SDC) */
-#define USD_CMD16	(0x40+16)	/* SET_BLOCKLEN */
-#define USD_CMD17	(0x40+17)	/* READ_SINGLE_BLOCK */
-#define USD_CMD18	(0x40+18)	/* READ_MULTIPLE_BLOCK */
-#define USD_CMD23	(0x40+23)	/* SET_BLOCK_COUNT (MMC) */
-#define	USD_ACMD23	(0xC0+23)	/* SET_WR_BLK_ERASE_COUNT (SDC) */
-#define USD_CMD24	(0x40+24)	/* WRITE_BLOCK */
-#define USD_CMD25	(0x40+25)	/* WRITE_MULTIPLE_BLOCK */
-#define USD_CMD55	(0x40+55)	/* APP_CMD */
-#define USD_CMD58	(0x40+58)	/* READ_OCR */
+#define MICROSD_CMD0	(0x40+0)	/* GO_IDLE_STATE */
+#define MICROSD_CMD1	(0x40+1)	/* SEND_OP_COND (MMC) */
+#define	MICROSD_ACMD41	(0xC0+41)	/* SEND_OP_COND (SDC) */
+#define MICROSD_CMD8	(0x40+8)	/* SEND_IF_COND */
+#define MICROSD_CMD9	(0x40+9)	/* SEND_CSD */
+#define MICROSD_CMD10	(0x40+10)	/* SEND_CID */
+#define MICROSD_CMD12	(0x40+12)	/* STOP_TRANSMISSION */
+#define MICROSD_ACMD13	(0xC0+13)	/* SD_STATUS (SDC) */
+#define MICROSD_CMD16	(0x40+16)	/* SET_BLOCKLEN */
+#define MICROSD_CMD17	(0x40+17)	/* READ_SINGLE_BLOCK */
+#define MICROSD_CMD18	(0x40+18)	/* READ_MULTIPLE_BLOCK */
+#define MICROSD_CMD23	(0x40+23)	/* SET_BLOCK_COUNT (MMC) */
+#define	MICROSD_ACMD23	(0xC0+23)	/* SET_WR_BLK_ERASE_COUNT (SDC) */
+#define MICROSD_CMD24	(0x40+24)	/* WRITE_BLOCK */
+#define MICROSD_CMD25	(0x40+25)	/* WRITE_MULTIPLE_BLOCK */
+#define MICROSD_CMD55	(0x40+55)	/* APP_CMD */
+#define MICROSD_CMD58	(0x40+58)	/* READ_OCR */
+
+
+#include "../../version/ver_mdriver_ram.h"
+#if VER_MDRIVER_RAM_MAJOR != 1 || VER_MDRIVER_RAM_MINOR != 2
+ #error Incompatible MDRIVER_RAM version number!
+#endif
 
 static int sd_readsector ( F_DRIVER * driver, void * data, unsigned long sector ) {
 	//TODO:implement sd_readsector
@@ -40,8 +55,8 @@ static void sd_release ( F_DRIVER * driver ) {
 }
 F_DRIVER * sd_initfunc ( unsigned long driver_param ) {
 	//TODO:implement sd_initfunc
-  /* Enabling clock to USART 0*/
-	CMU_ClockEnable(cmuClock_USART0, true);
-	SPI_setup(USART0_NUM, GPIO_POS1, false);
+	/* Enabling clock to USART 0*/
+	//CMU_ClockEnable(cmuClock_USART0, true);
+	//SPI_setup(USART0_NUM, GPIO_POS1, false);
 	return NULL;
 }
