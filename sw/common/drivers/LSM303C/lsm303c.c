d828dac7b931e61294fab42de726b6abbfdda915
Grzegorz Daniluk <grzegorz.daniluk@cern.ch>
2014-08-12 21:24:27 +0200

    lsm303c: unified minimalistic driver for both accelerometer and compass

--- sw/common/drivers/LSM303C/lsm303c.c
+++ sw/common/drivers/LSM303C/lsm303c.c
@@ -35,7 +35,7 @@ static void spi_init()
   GPIO_PinModeSet(gpioPortD,  4, gpioModeInput, 0);    /* Mag drdy */
 }
 
-void spi_send(uint8_t dev, uint8_t adr, uint8_t dat)
+int spi_send(uint8_t dev, uint8_t adr, uint8_t dat)
 {
   USART_TypeDef *spi = SPI_USART;
 
@@ -59,10 +59,12 @@ void spi_send(uint8_t dev, uint8_t adr, uint8_t dat)
   /* both CS up */
   GPIO_PinOutSet(gpioPortC, ACC_CS_PIN);
   GPIO_PinOutSet(gpioPortC, MAG_CS_PIN);
+
+  return LSM303_SUCCESS;
 }
 
 /* read byte from given a address */
-uint8_t spi_read(uint8_t dev, uint8_t adr)
+int spi_read(uint8_t dev, uint8_t adr, uint8_t *dat)
 {
   USART_TypeDef *spi = SPI_USART;
 
@@ -96,19 +98,260 @@ uint8_t spi_read(uint8_t dev, uint8_t adr)
   GPIO_PinOutSet(gpioPortC, ACC_CS_PIN);
   GPIO_PinOutSet(gpioPortC, MAG_CS_PIN);
 
-  return spi->RXDATA & 0xff;
+  *dat = (spi->RXDATA & 0xff);
+  return LSM303_SUCCESS;
 }
 
 /************************************************/
-/*                LSM303C stuff                 */
+/*                LSM303 stuff                 */
 /************************************************/
 int lsm303_init()
 {
   spi_init();
 
   /*setup 3-wire SPI for Accelerometer & Magnetometer*/
-  LSM303C_ACC_SerialInterfaceMode(LSM303C_ACC_SIM_3WIRE_INTERFACE);
+  //LSM303_ACC_SerialInterfaceMode(LSM303_ACC_SIM_3WIRE_INTERFACE);
+	lsm303_serialmode(DEV_ACC, LSM303_ACC_3WIRE);
+	//lsm303_serialmode(DEV_MAG, LSM303_ACC_3WIRE);
 
+  lsm303_enableaxis(DEV_ACC, 0x7); 
+  lsm303_fifo_mode(DEV_ACC, LSM303_FMODE_BYPASS, 1);
+  lsm303_fifo_mode(DEV_ACC, LSM303_FMODE_BYPASS, 0);
+  lsm303_odr(DEV_ACC, LSM303_ACC_ODR_10_Hz);
 
   return LSM303_SUCCESS;
+}
+
+/* dev: DEV_ACC/DEV_MAG
+ * en: 1->enable; 0->disable
+ * dir: 1->positive; 0->negative (for ACC) */
+int lsm303_selftest(int dev, int en, int dir)
+{
+  uint8_t val;
+
+  if( (dev==DEV_ACC && !spi_read(dev, LSM303_CTRL5, &val)) ||
+      (dev==DEV_MAG && !spi_read(dev, LSM303_CTRL1, &val)) )
+    return LSM303_ERROR;
+
+  val &= ~( dev==DEV_ACC ? LSM303_ACC_ST_MASK : LSM303_MAG_ST_MASK );
+
+  if (dev==DEV_ACC && en && dir)
+    val |= LSM303_ACC_ST_POS;
+  else if (dev==DEV_ACC && en && !dir)
+    val |= LSM303_ACC_ST_NEG;
+  else if (dev==DEV_MAG && en)
+    val |= LSM303_MAG_ST_EN;
+
+  if( (dev==DEV_ACC && !spi_send(dev, LSM303_CTRL5, val)) ||
+      (dev==DEV_MAG && !spi_send(dev, LSM303_CTRL1, val)) )
+    return LSM303_ERROR;
+
+  return LSM303_SUCCESS;
+}
+
+/* Set output data rate
+ * dev: DEV_ACC/DEV_MAG
+ * odr: appropriate constant from lsm303.h */
+int lsm303_odr(int dev, LSM303_ODR_t odr)
+{
+	uint8_t val;
+
+  if (!spi_read(dev, LSM303_CTRL1, &val))
+    return LSM303_ERROR;
+
+  val &= ~( dev==DEV_ACC ? LSM303_ACC_ODR_MASK : LSM303_MAG_ODR_MASK );
+  val |= odr;
+
+  if (!spi_send(dev, LSM303_CTRL1, val))
+    return LSM303_ERROR;
+
+  return LSM303_SUCCESS;
+}
+
+/* set operation mode
+ * dev: DEV_ACC/DEV_MAG
+ * opm: constant from lsm303.h */
+int lsm303_opmode(int dev, LSM303_OPM_t opm)
+{
+	uint8_t val;
+
+	if (!spi_read(dev, LSM303_CTRL1, &val))
+		return LSM303_ERROR;
+
+  val &= ~( dev==DEV_ACC ? LSM303_ACC_OPM_MASK : LSM303_MAG_OPM_MASK );
+  val |= opm;
+
+  if( !spi_send(dev, LSM303_CTRL1, val) )
+    return LSM303_ERROR;
+
+	return LSM303_SUCCESS;
+}
+
+/* soft reset
+ * dev: DEV_ACC/DEV_MAG
+ * hard: 1-> reboot, 0-> soft reset */
+int lsm303_rst(int dev, int hard)
+{
+	uint8_t val;
+
+	if( (dev==DEV_ACC && !hard && !spi_read(dev, LSM303_CTRL5, &val)) ||
+			(dev==DEV_ACC && hard  && !spi_read(dev, LSM303_CTRL6, &val)) ||
+			(dev==DEV_MAG && !spi_read(dev, LSM303_CTRL2, &val)) )
+		return LSM303_ERROR;
+
+	if( hard ) 
+		val |= (dev==DEV_ACC ? LSM303_ACC_REBOOT : LSM303_MAG_REBOOT);
+	else
+		val |= (dev==DEV_ACC ? LSM303_ACC_SRST : LSM303_MAG_SRST);
+
+  if( (dev==DEV_ACC && !hard && !spi_send(dev, LSM303_CTRL5, val)) ||
+			(dev==DEV_ACC && hard  && !spi_send(dev, LSM303_CTRL6, val)) ||
+      (dev==DEV_MAG && !spi_send(dev, LSM303_CTRL2, val)) )
+    return LSM303_ERROR;
+
+  return LSM303_SUCCESS;
+}
+
+/* temperature readout */
+int lsm303_temp(int16_t *temp)
+{
+	uint8_t val;
+	*temp = 0;
+
+	/*enable temp sensor*/
+	if( !spi_read(DEV_MAG, LSM303_CTRL1, &val) )
+		return LSM303_ERROR;
+	val |= LSM303_MAG_TEMP_EN;
+	if( !spi_send(DEV_MAG, LSM303_CTRL1, val) )
+		return LSM303_ERROR;
+
+	/*read temperature*/
+	if( !spi_read(DEV_MAG, LSM303_MAG_TEMP_L, &val) )
+		return LSM303_ERROR;
+	*temp = (int16_t)val;
+	if( !spi_read(DEV_MAG, LSM303_MAG_TEMP_H, &val) )
+		return LSM303_ERROR;
+	*temp |= ((int16_t)val << 8) & 0xFF00;
+
+	/*disable temp sensor*/
+	if( !spi_read(DEV_MAG, LSM303_CTRL1, &val) )
+		return LSM303_ERROR;
+	val &= ~LSM303_MAG_TEMP_EN;
+	if( !spi_send(DEV_MAG, LSM303_CTRL1, val) )
+		return LSM303_ERROR;
+
+	return LSM303_SUCCESS;
+}
+
+int lsm303_fullscale(int dev, LSM303_FS_t fs)
+{
+	uint8_t val;
+
+	/*check if _fs_ for proper device*/
+	if( (dev==DEV_ACC && !(LSM303_ACC_FS_MASK & fs)) ||
+			(dev==DEV_MAG && !(LSM303_MAG_FS_MASK & fs)) )
+		return LSM303_ERROR;
+
+	if( (dev==DEV_ACC && !spi_read(dev, LSM303_CTRL4, &val)) ||
+			(dev==DEV_MAG && !spi_read(dev, LSM303_CTRL2, &val)) )
+		return LSM303_ERROR;
+
+	val &= ~(dev==DEV_ACC ? LSM303_ACC_FS_MASK : LSM303_MAG_FS_MASK);
+	val |= fs;
+
+	if( (dev==DEV_ACC && !spi_send(dev, LSM303_CTRL4, val)) ||
+			(dev==DEV_MAG && !spi_send(dev, LSM303_CTRL2, val)) )
+		return LSM303_ERROR;
+
+	return LSM303_SUCCESS;
+}
+
+int lsm303_serialmode(int dev, LSM303_SMODE_t mode)
+{
+	uint8_t val;
+
+	if( (dev==DEV_ACC && !spi_read(dev, LSM303_CTRL4, &val)) ||
+			(dev==DEV_MAG && !spi_read(dev, LSM303_CTRL3, &val)) )
+		return LSM303_ERROR;
+
+	val &= ~(dev==DEV_ACC ? LSM303_ACC_3WIRE : LSM303_MAG_3WIRE);
+	val |= mode;
+
+	if( (dev==DEV_ACC && !spi_send(dev, LSM303_CTRL4, val)) ||
+			(dev==DEV_MAG && !spi_send(dev, LSM303_CTRL3, val)) )
+		return LSM303_ERROR;
+
+	return LSM303_SUCCESS;
+}
+
+int lsm303_enableaxis(int dev, int mask)
+{
+	uint8_t val;
+
+	if( dev == DEV_MAG )
+		return LSM303_ERROR;
+
+	if(!spi_read(dev, LSM303_CTRL1, &val))
+		return LSM303_ERROR;
+
+	val &= ~LSM303_ACC_AXIS_MASK;
+	val |= mask;
+
+	if(!spi_send(dev, LSM303_CTRL1, val))
+		return LSM303_ERROR;
+
+	return LSM303_SUCCESS;
+}
+
+int lsm303_fifo_mode(int dev, LSM303_FMODE_t mode, int en)
+{
+	uint8_t val;
+
+	if(dev==DEV_MAG)
+		return LSM303_ERROR;
+
+	/*first enable/disable fifo*/
+	if(!spi_read(dev, LSM303_CTRL3, &val))
+		return LSM303_ERROR;
+	if(en)
+		val |= LSM303_FIFO_EN;
+	else
+		val &= ~LSM303_FIFO_EN;
+	if(!spi_send(dev, LSM303_CTRL3, val))
+		return LSM303_ERROR;
+
+	/* don't set mode if FIFO disabled */
+	if(!en) return LSM303_SUCCESS;
+
+	/*now set the mode*/
+	if(!spi_read(dev, LSM303_ACC_FIFO_CTRL, &val))
+		return LSM303_ERROR;
+	val &= ~LSM303_FMODE_MASK;
+	val |= mode;
+	if(!spi_send(dev, LSM303_ACC_FIFO_CTRL, val))
+		return LSM303_ERROR;
+
+	return LSM303_SUCCESS;
+}
+
+int lsm303_get_sample(int dev, int16_t *x, int16_t *y, int16_t *z)
+{
+	uint8_t val_l, val_h;
+
+	if( !spi_read(dev, LSM303_OUT_X_L, &val_l) ||
+			!spi_read(dev, LSM303_OUT_X_H, &val_h) )
+		return LSM303_ERROR;
+	*x = (int16_t) ((val_h << 8) | val_l);
+
+	if( !spi_read(dev, LSM303_OUT_Y_L, &val_l) ||
+			!spi_read(dev, LSM303_OUT_Y_H, &val_h) )
+		return LSM303_ERROR;
+	*y = (int16_t) ((val_h << 8) | val_l);
+
+	if( !spi_read(dev, LSM303_OUT_Z_L, &val_l) ||
+			!spi_read(dev, LSM303_OUT_Z_H, &val_h) )
+		return LSM303_ERROR;
+	*z = (int16_t) ((val_h << 8) | val_l);
+
+	return LSM303_SUCCESS;
 }
