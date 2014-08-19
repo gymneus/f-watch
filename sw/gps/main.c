d945b39c0b67eb203d8c386e19d52f66c511df5c
Theodor Stana <t.stana@cern.ch>
2014-08-19 14:26:16 +0200

    Can now receive data from GPS

--- sw/gps/main.c
+++ sw/gps/main.c
@@ -51,35 +51,18 @@ volatile char rxbuf[RXBUFSIZE] = {'0', '1', '2', '3', '4', '5', '6', '7'};
 volatile char idx = 0;
 
 static void gps_init();
+static void gps_on_off_pulse();
 
-//void LEUART0_IRQHandler()
-//{
-//        if (LEUART0->IF & LEUART_IF_RXDATAV) {
-//                rxbuf[idx++] = LEUART_Rx(LEUART0);
-//                if (idx == RXBUFSIZE) {
-//                        idx = 0;
-//                        USBD_Write(USBDESC_EP_DATA_OUT, rxbuf, RXBUFSIZE, NULL);
-//                }
-//        }
-//}
-
-//void GPIO_EVEN_IRQHandler()
-//{
-//        uint32_t intline = GPIO_IntGet();
-//        GPIO_IntClear(1 << intline);
-//        if (intline == 0) USBD_Write(USBDESC_EP_DATA_OUT, "SW_TR\r\n", 6, NULL);
-//        if (intline == 8) USBD_Write(USBDESC_EP_DATA_OUT, "SW_BR\r\n", 6, NULL);
-//        if (intline == 6) USBD_Write(USBDESC_EP_DATA_OUT, "SW_BL\r\n", 6, NULL);
-//        GPIO_PinOutSet(gpioPortE, 11);
-//}
-//
-//void GPIO_ODD_IRQHandler()
-//{
-//        uint32_t intline = GPIO_IntGet();
-//        GPIO_IntClear(1 << intline);
-//        if (intline == 7) USBD_Write(USBDESC_EP_DATA_OUT, "SW_TL\r\n", 6, NULL);
-//        GPIO_PinOutSet(gpioPortE, 11);
-//}
+void LEUART0_IRQHandler()
+{
+        if (LEUART0->IF & LEUART_IF_RXDATAV) {
+                rxbuf[idx++] = LEUART_Rx(LEUART0);
+                if (idx == RXBUFSIZE) {
+                        idx = 0;
+                        USBD_Write(USBDESC_EP_DATA_OUT, rxbuf, RXBUFSIZE, NULL);
+                }
+        }
+}
 
 int main()
 {
@@ -92,35 +75,30 @@ int main()
 
         USBD_Init(&initstruct);
 
-        //gps_init();
-//  /* Configure interrupt pin as input with pull-up */
-//  GPIO_PinModeSet(gpioPortA, 0, gpioModeInputPull, 1);
-//  GPIO_PinModeSet(gpioPortA, 8, gpioModeInputPull, 1);
-//  GPIO_PinModeSet(gpioPortC, 6, gpioModeInputPull, 1);
-//  GPIO_PinModeSet(gpioPortC, 7, gpioModeInputPull, 1);
-//
-//  /* Set falling edge interrupt and clear/enable it */
-//  GPIO_IntConfig(gpioPortA, 0, false, true, true);
-//  GPIO_IntConfig(gpioPortA, 8, false, true, true);
-//  GPIO_IntConfig(gpioPortC, 6, false, true, true);
-//  GPIO_IntConfig(gpioPortC, 7, false, true, true);
-//
-//  NVIC_ClearPendingIRQ(GPIO_EVEN_IRQn);
-//  NVIC_ClearPendingIRQ(GPIO_ODD_IRQn);
-//  NVIC_EnableIRQ(GPIO_EVEN_IRQn);
-//  NVIC_EnableIRQ(GPIO_ODD_IRQn);
+        gps_init();
+
+        USBD_Write(USBDESC_EP_DATA_OUT, "init done!\r\n", 12, NULL);
 
         int i;
-        int x = 0;
-
-        char *tmp = "now will you work?\r\n";
 
         for (;;) {
-                for (i = 0; i < 10000000; i++) ;
-                x ^= 1;
-                x ? GPIO_PinOutSet(gpioPortE, 11) :
-                    GPIO_PinOutClear(gpioPortE, 11);
-                USBD_Write(USBDESC_EP_DATA_OUT, (void *)tmp, strlen(tmp), NULL);
+                for (i = 0; i < 3000000; i++) {
+                        GPIO_PinInGet(gpioPortA, 1) ?
+                                GPIO_PinOutSet(gpioPortE, 11) :
+                                GPIO_PinOutClear(gpioPortE, 11);
+                }
+                USBD_Write(USBDESC_EP_DATA_OUT, "\r\nOFF\r\n", 7, NULL);
+                gps_on_off_pulse();
+
+
+                for (i = 0; i < 3000000; i++) {
+                        GPIO_PinInGet(gpioPortA, 1) ?
+                                GPIO_PinOutSet(gpioPortE, 11) :
+                                GPIO_PinOutClear(gpioPortE, 11);
+                }
+                USBD_Write(USBDESC_EP_DATA_OUT, "\r\nON\r\n", 6, NULL);
+                gps_on_off_pulse();
+
         }
 
         return 0;
@@ -128,24 +106,20 @@ int main()
 
 static void gps_init()
 {
-        int i;
+        /* GPS reset pin */
+        USB->ROUTE &= ~(USB_ROUTE_VBUSENPEN);
+        GPIO_PinModeSet(gpioPortF, 5, gpioModePushPull, 1);
 
-        /*-------------------------------------------------*
-         * ON_OFF pulse
-         *-------------------------------------------------*/
+        /* GPS WAKUP pin */
+        GPIO_PinModeSet(gpioPortA, 1, gpioModeInput, 0);
+
+        /* ON_OFF _double_ pulse */
         GPIO_PinModeSet(gpioPortE, 13, gpioModePushPull, 0);
-        GPIO_PinOutSet(gpioPortE, 13);
-        GPIO_PinOutSet(gpioPortE, 12);
-        for (i = 0; i < 100000; i++)
-                ;
-        GPIO_PinOutClear(gpioPortE, 13);
-        GPIO_PinOutClear(gpioPortE, 12);
+        gps_on_off_pulse();
+        gps_on_off_pulse();
 
-        /*-------------------------------------------------*
-         * LEUART0 config
-         *-------------------------------------------------*/
-        LEUART_TypeDef      *leuart = LEUART0;
-        LEUART_Init_TypeDef init    = LEUART_INIT_DEFAULT;
+        /* LEUART0 config */
+        LEUART_Init_TypeDef init = LEUART_INIT_DEFAULT;
 
         /* To avoid false start, configure output as high */
         GPIO_PinModeSet(gpioPortE, 14, gpioModePushPull, 1);
@@ -159,20 +133,35 @@ static void gps_init()
         init.enable = leuartDisable;
         init.baudrate = 4800;
         init.databits = leuartDatabits8;
-        init.stopbits = leuartStopbits2;
+        init.stopbits = leuartStopbits1;
 
-        LEUART_Init(leuart, &init);
+        LEUART_Init(LEUART0, &init);
 
-        leuart->ROUTE = LEUART_ROUTE_RXPEN |
-                        LEUART_ROUTE_TXPEN |
-                        LEUART_ROUTE_LOCATION_LOC2;
+        LEUART0->ROUTE = LEUART_ROUTE_RXPEN |
+                         LEUART_ROUTE_TXPEN |
+                         LEUART_ROUTE_LOCATION_LOC2;
 
-        LEUART_IntClear(leuart, LEUART_IF_RXDATAV);
+        LEUART_IntClear(LEUART0, LEUART_IF_RXDATAV);
         NVIC_ClearPendingIRQ(LEUART0_IRQn);
 
-        LEUART_IntEnable(leuart, LEUART_IF_RXDATAV);
+        LEUART_IntEnable(LEUART0, LEUART_IF_RXDATAV);
         NVIC_EnableIRQ(LEUART0_IRQn);
 
-        LEUART_Enable(leuart, leuartEnable);
-        GPIO_PinOutSet(gpioPortE, 11);
+        LEUART_Enable(LEUART0, leuartEnable);
+
+}
+
+static void gps_on_off_pulse()
+{
+        int i;
+        /* Pulse */
+        GPIO_PinOutSet(gpioPortE, 13);
+        GPIO_PinOutSet(gpioPortE, 12);
+        for (i = 0; i < 100000; i++)
+                ;
+        GPIO_PinOutClear(gpioPortE, 13);
+        GPIO_PinOutClear(gpioPortE, 12);
+        /* Delay to make sure GPS module sees this as a pulse */
+        for (i = 0; i < 100000; i++)
+                ;
 }
