0190d7f88959d1eb17c7776554fb5f2811c2e792
Matthieu Cattin <matthieu.cattin@cern.ch>
2014-09-24 22:01:37 +0200

    sw: Working on a path drawing widget (for gps app).

---
+++ sw/ui-test/path_widget.h
@@ -0,0 +1,15 @@
+#include "ui.h"
+#include "math.h"
+
+
+extern struct ui_widget path_widget;
+
+typedef struct path{
+        int32_t* d;      // pointer on data array
+        uint16_t d_size; // data array size
+        uint8_t x_size;  // X plot size (pixel)
+        uint8_t y_size;  // Y plot size (pixel)
+        char* title;     // pointer on plot title string
+        uint8_t frame;   // draw frame around plot
+        uint8_t line;    // draw lines between data points
+}path;
