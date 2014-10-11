0b340388ccc73ff830e218962faf3727820d327d
Maciej Suminski <maciej.suminski@cern.ch>
2014-10-11 18:43:27 +0200

    Removed unused files.

--- sw/common/pp_printf/pp-printf.h
+++
@@ -1,8 +0,0 @@
-#include <stdarg.h>
-
-extern int pp_sprintf(char *s, const char *fmt, ...)
-        __attribute__((format(printf,2,3)));
-
-extern int pp_vsprintf(char *buf, const char *, va_list)
-        __attribute__ ((format (printf, 2, 0)));
-
